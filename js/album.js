667f0dc62425612c59e3eee29cc3ab30abd2358a
crislmfroes <cris.lima.froes@gmail.com>
2018-03-19 22:17:44 -0300

    Mensagem

--- js/album.js
+++ js/album.js
@@ -3,10 +3,17 @@ const images = document.querySelectorAll('.photo');
 const zoomImg = document.querySelector('#zoom');
 const close = document.querySelector('.close');
 
+function zoom(element) {
+    modal.style.display = "block";
+    zoomImg.src = element.src;
+}
+
 for (image of images) {
     image.addEventListener('click', function() {
-        modal.style.display = "block";
-        zoomImg.src = this.src;
+        zoom(this);
+    });
+    image.addEventListener('touchstart', function() {
+        zoom(this);
     });
 }
 
