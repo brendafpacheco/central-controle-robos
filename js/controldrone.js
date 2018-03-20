e49bf3d226ef6434a3b59463694cdebc8b646f0c
crislmfroes <cris.lima.froes@gmail.com>
2018-03-19 21:31:04 -0300

    Mensagem

--- js/controldrone.js
+++ js/controldrone.js
@@ -2,16 +2,20 @@ const locationButton = document.querySelector('#location');
 const form = document.querySelector('form');
 
 function fillForm(position) {
-    //console.log(position);
-    //console.log('ok!');
-    form.altitude.value = position.coords.altitude;
+    console.dir(position);
+    console.log('ok!');
+    console.dir(form.altitude);
+    form.altitude.value = 42;
     form.latitude.value = position.coords.latitude;
     form.longitude.value = position.coords.longitude;
 }
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(fillForm);
+        navigator.geolocation.getCurrentPosition(fillForm, function (error) {
+            console.log(error);
+        });
+        console.log('ok!');
     }
 }
 
